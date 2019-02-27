8e0d4aac231123affba39d2bfd5eb2aa3725f9d6
SoinSoin <vincent.retore@gmail.com>
2019-02-27 16:18:45 +0100

    modify name attr type

--- ourflow/api/admin.py
+++ ourflow/api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-# from .models import * as DbTb
-# admin.site.register(DbTb)
+from .models import * 
+
+admin.site.register(Page)
+admin.site.register(Item)
+admin.site.register()
+admin.site.register(Paragraph)
+admin.site.register(PagePara)
+admin.site.register(ParaItem)
--- ourflow/api/models/Types.py
+++ ourflow/api/models/Types.py
@@ -1,4 +1,4 @@
 from django.db import models
 
 class Type(models.Model):
-    name_para = models.CharField(max_length=30, db_index=True, null=True)
+    name_type = models.CharField(max_length=30, db_index=True, null=True)
