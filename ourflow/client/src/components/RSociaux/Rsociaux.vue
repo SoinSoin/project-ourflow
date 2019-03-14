7944de3dd8ab730c83a04e4a1277330423f8662c
Philippine GILABERT <philou@macbook-pro-de-philippine.home>
2019-03-14 21:34:44 +0100

    prob dependance ok

--- ourflow/client/src/components/RSociaux/Rsociaux.vue
+++ ourflow/client/src/components/RSociaux/Rsociaux.vue
@@ -1,9 +1,8 @@
 <template>
   <div>
-    <fa-icon :icon="{ prefix: 'fab', iconName: 'facebook-square' }" size ="5x"/> <!-- https://fontawesome.com/icons/facebook-square?style=brands -->
-    <!-- j'ai installé les librairies manquante et nommé le composant voir main.js -->
-    <!-- <font-awesome-icon :icon="{ prefix: 'fab', iconName: 'facebook' }"/> -->
-    <!-- <font-awesome-icon :icon="{ prefix: 'fab', iconName: 'instagram' }"/> -->
+    <fa-icon :icon="{ prefix: 'fab', iconName: 'facebook-f' }" size="5x"/>
+    <fa-icon :icon="{ prefix: 'fab', iconName: 'instagram' }" size="5x"/>
+    <fa-icon :icon="{ prefix: 'fab', iconName: 'twitter' }" size="5x"/>
   </div>
 </template>
 
