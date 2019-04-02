f8fa9c44e62768a25c00c08719ae2beb813d7cba
SoinSoin <vincent.retore@gmail.com>
2019-04-02 09:25:33 +0200

    modify backend for delete items == null

--- ourflow/api/serializers/ParaItemsSerializer.py
+++ ourflow/api/serializers/ParaItemsSerializer.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from api.models import ParaItem, Item
 from .ItemsSerializer import ItemSerializer
+import json
 from rest_framework.response import Response
 
 class ParaItemSerializer(serializers.ModelSerializer):
@@ -14,4 +15,9 @@ class ParaItemSerializer(serializers.ModelSerializer):
         context = self.context['request']
         qset = Item.objects.filter(id=obj.item_id)
         json_raw = [ItemSerializer(map_qset, context={'request':context}).data for map_qset in qset]
+        parse_json_list = json.dumps(json_raw[0])
+        parse_py = json.loads(parse_json_list)
+        for key in parse_py: 
+            if parse_py[key]==None:
+                del json_raw[0][key]
         return json_raw[0]
